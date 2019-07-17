b7e513720c3e4428cb29296700d8d4215929a212
rebekasultana <rebekasultana5454@gmail.com>
2019-07-17 21:28:27 +0600

    simple java

---
+++ multiple.java
@@ -0,0 +1,11 @@
+public class Practice {
+
+	public static void main(String[] args) {
+		int first = 10;
+        int second = 20;
+        int multiple = first * second;
+        System.out.println("The multiple is: " +multiple);
+    		
+	}
+
+}
---
+++ sub.java
@@ -0,0 +1,11 @@
+public class Practice {
+
+	public static void main(String[] args) {
+		int first = 10;
+        int second = 20;
+        int sub = first - second;
+        System.out.println("The subtract is: " + sub);
+    		
+	}
+
+}
